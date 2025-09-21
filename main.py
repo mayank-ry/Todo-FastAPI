072b2f03af9dd17edc9ed00088fb7d83d2d7e761
Darsh Rawat <rawatdarsh@gmail.com>
2025-09-21 19:50:33 +0530

    Database Initial Setup

---
+++ main.py
@@ -0,0 +1,27 @@
+# database.py
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create the engine (talks to DB)
+engine = create_engine(DATABASE_URL) # type: ignore
+
+# SessionLocal: actual session class bound to engine
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base: used for creating ORM models
+Base = declarative_base()
+
+# Dependency for getting DB session inside routes
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
